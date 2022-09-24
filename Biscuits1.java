0efd0c1056a7023a7a3bdd56594feffc84d72d96
naveen74111 <naveen.xworkz@gmail.com>
2022-09-24 21:22:19 +0530

    array files

---
+++ Biscuits1.java
@@ -0,0 +1,14 @@
+class Biscuits1
+{
+	public static void main(String a[])
+	{
+	 String biscuits []={"gooday","parle g","unibic","patanjali","mariegold","happy"};
+	 
+	System.out.println("size of biscuits" + biscuits.length);
+	
+	for(int i = 0; i < biscuits.length; i++)
+	{
+		System.out.println(biscuits[i]+ " ");
+	}
+	}
+}
---
+++ Cloths.java
@@ -0,0 +1,15 @@
+class Cloths
+{
+public static void main(String a[])
+{
+String cloths[]={"mens","kids","womens"};
+	String nave[]={"","",90};
+	{
+		System.out.println("Size of cloths" + cloths.length);
+	}
+for(int c=0;c<cloths.length;c++)
+{
+System.out.println( cloths[c]+" ");
+}
+}
+}
